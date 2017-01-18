4a25d3cf3cfd0b2a6ba770b6a9b163bede678804
Sangeethshiv36 <b.sangeethsivan@gmail.com>
2017-01-18 23:06:55 +0530

    Update:Reverse Logic

--- logi/src/com/sangeeth/ReverseArray.java
+++ logi/src/com/sangeeth/ReverseArray.java
@@ -1,20 +1,22 @@
 package com.sangeeth;
 
-class A { 
+class A {
 
-	int[] arr = { 1, 2, 3, 4, 5 };
-	int a = arr.length-1;
+	int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+	int a = arr.length - 1;
 
 	void revArr(int[] arr) {
 		System.out.println(arr[a]);
 		a--;
-		revArr(arr);
+		if (a >= 0) {
+			revArr(arr);
+		}
 	}
 }
 
-public class ReverseArray { 
-	public static void main(String[] args){
-		A a=new A();
+public class ReverseArray {
+	public static void main(String[] args) {
+		A a = new A();
 		a.revArr(a.arr);
 	}
 
