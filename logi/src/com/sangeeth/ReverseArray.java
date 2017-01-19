58d731f4869c1a9f1fe269dfc0f4629e471510c4
Sangeethshiv36 <b.sangeethsivan@gmail.com>
2017-01-19 10:48:01 +0530

    Update: Optimized

--- logi/src/com/sangeeth/ReverseArray.java
+++ logi/src/com/sangeeth/ReverseArray.java
@@ -1,23 +1,19 @@
 package com.sangeeth;
 
-class A {
 
-	int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-	int a = arr.length - 1;
+public class ReverseArray {
+	static	int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+	static int a = arr.length - 1;
 
-	void revArr(int[] arr) {
+	static void revArr(int[] arr) {
 		System.out.println(arr[a]);
 		a--;
 		if (a >= 0) {
 			revArr(arr);
 		}
 	}
-}
-
-public class ReverseArray {
 	public static void main(String[] args) {
-		A a = new A();
-		a.revArr(a.arr);
+		revArr(arr);
 	}
 
 }
